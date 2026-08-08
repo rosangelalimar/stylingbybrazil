4234fc8964b1275beb319fb48346e5b7f1184a68
rosangelalimar <rosangelalimars545@gmail.com>
2026-08-08 13:59:55 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -20,29 +21,34 @@ import Fettreducering from './pages/Fettreducering';
 
 function App() {
   return (
-    <div className="relative min-h-screen bg-white">
+    <>
       <ScrollToTop />
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/frisor" element={<Frisor />} />
-        <Route path="/slingor" element={<Slingor />} />
-        <Route path="/fargning" element={<Fargning />} />
-        <Route path="/keratin" element={<Keratin />} />
-        <Route path="/ogon-bryn" element={<OgonBryn />} />
-        <Route path="/vaxning" element={<Vaxning />} />
-        <Route path="/laser" element={<Laser />} />
-        <Route path="/behandlingar" element={<Behandlingar />} />
-        <Route path="/kroppsbehandling" element={<Kroppsbehandling />} />
-        <Route path="/ansiktsbehandling" element={<Ansiktsbehandling />} />
-        <Route path="/fettreducering" element={<Fettreducering />} />
-        <Route path="/priser" element={<Priser />} />
-        <Route path="/team/:slug" element={<TeamMemberPage />} />
-        {/* Catch all route - redirect to home */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/frisor" element={<Frisor />} />
+          <Route path="/slingor" element={<Slingor />} />
+          <Route path="/fargning" element={<Fargning />} />
+          <Route path="/keratin" element={<Keratin />} />
+          <Route path="/ogon-bryn" element={<OgonBryn />} />
+          <Route path="/vaxning" element={<Vaxning />} />
+          <Route path="/laser" element={<Laser />} />
+          <Route path="/behandlingar" element={<Behandlingar />} />
+          <Route path="/kroppsbehandling" element={<Kroppsbehandling />} />
+          <Route path="/ansiktsbehandling" element={<Ansiktsbehandling />} />
+          <Route path="/fettreducering" element={<Fettreducering />} />
+          <Route path="/priser" element={<Priser />} />
+          <Route path="/team/:slug" element={<TeamMemberPage />} />
+
+          {/* Catch-all route */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
+
       <Footer />
-    </div>
+    </>
   );
 }
 
